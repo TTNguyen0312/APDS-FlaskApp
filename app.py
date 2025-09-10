3d61ac934549a2e59c50c4aa2df0dec94a1bf084
Audrey175 <s3975091@rmit.edu.vn>
2025-09-10 20:29:03 +0700

    feat: Test database connect

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify
+import os
 
 app = Flask(__name__)
 
+db_path = os.path.join(os.path.dirname(__file__), 'sdms.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Clothes(db.Model):
+    ClothingID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ClothTitle = db.Column(db.String, nullable=False)
+    ClothDescription = db.Column(db.Text, nullable=True)
+    Department = db.Column(db.String, nullable=True)
+    ClassName = db.Column(db.String, nullable=True)
+    DivisionName = db.Column(db.String, nullable=True)
+    PositiveFeedbackCount = db.Column(db.Integer, default=0)
+
+    def json(self):
+        return {
+            'ClothingID': self.ClothingID,
+            'ClothTitle': self.ClothTitle,
+            'ClothDescription': self.ClothDescription,
+            'Department': self.Department,
+            'ClassName': self.ClassName,
+            'DivisionName': self.DivisionName,
+            'PositiveFeedbackCount': self.PositiveFeedbackCount
+        }
+
 @app.route('/')
-def home():
-    return render_template('landingPage.html')
+def index():
+    clothes = Clothes.query.all()
+    clothes = [cloth.json() for cloth in clothes]
+    return clothes
+
+@app.route('/add', methods=['POST'])
+def add_cloth():
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form  # fallback to form data
+
+    new_item = Clothes(
+        ClothTitle=data.get('ClothTitle'),
+        ClothDescription=data.get('ClothDescription'),
+        Department=data.get('Department'),
+        ClassName=data.get('ClassName'),
+        DivisionName=data.get('DivisionName'),
+        PositiveFeedbackCount=data.get('PositiveFeedbackCount', 0)
+    )
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    return jsonify(new_item.json()), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
