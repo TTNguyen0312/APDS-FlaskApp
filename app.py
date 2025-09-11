b024da2491bf4a0a122bdd31231f9944e62f706d
Long Nguyen <s4002308@rmit.edu.vn>
2025-09-11 19:55:43 +0700

    search (currently hard coded for preview)

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
-    return render_template('landingPage.html')
+    return render_template("search.html")
+
+# give the same view an extra endpoint called 'search'
+app.add_url_rule("/search", endpoint="search", view_func=home)
 
 if __name__ == '__main__':
     app.run(debug=True)
