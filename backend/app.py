a93068dfd9dea99b15db722db02bc8c7eefae0b8
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2025-09-14 18:38:22 +0700

    fix: host="0.0.0.0"

--- backend/app.py
+++ backend/app.py
@@ -194,4 +194,4 @@ def add_review():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3000))  # default to 3000 locally
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
--- frontend/app.py
+++ frontend/app.py
@@ -167,4 +167,4 @@ def search():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
